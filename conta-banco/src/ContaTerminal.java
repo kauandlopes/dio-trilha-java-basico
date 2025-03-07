3ce6974c0e29c00a559b7394e917f69473ca8aa4
kauandlopes <lopeskauandias@gmail.com>
2025-03-07 18:43:06 -0300

    ContaBanco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        String agencia, nomeCliente = "Mario Andrade";
+        int numeroConta;
+        double saldo = 237.48;
+
+        Scanner sc = new Scanner(System.in);
+
+        //entrada de dados agencia
+        do { 
+            System.out.println("Por favor, digite o número da Agência! ");
+            agencia = sc.nextLine();
+        } while (!agencia.equals("067-8"));
+
+        //entrada de dados usuario
+        do { 
+            System.out.println("Agora digite um número de conta válido! ");
+            numeroConta = sc.nextInt();
+            System.out.println("");
+        } while (numeroConta != 1021);
+        
+        // Exibir mensagem 
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo R$" + saldo + " já está disponível para saque.");
+
+        sc.close();
     }
 }
